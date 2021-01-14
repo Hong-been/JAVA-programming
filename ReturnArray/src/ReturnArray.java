f7784f7d70e9147aaad656feef805fe70edf10c7
hongbxxn <redbean-2@naver.com>
2021-01-14 12:45:30 +0900

    Java Basic Programming

---
+++ ReturnArray/src/ReturnArray.java
@@ -0,0 +1,15 @@
+public class ReturnArray {
+	public static void main(String[] args) {
+		int intArray[];
+		intArray=makeArray();
+		for(int k:intArray)
+			System.out.println(k);
+	}
+	
+	static int[] makeArray() {
+		int temp[]= {0,1,2,3,4};
+		for(int k:temp)
+			System.out.println(k);
+		return temp;
+	}
+}
