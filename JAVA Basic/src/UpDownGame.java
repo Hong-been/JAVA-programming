682b779ddb8c347faf3b04749dfacbbd55c87384
Hongbxxn <65804460+Hongbxxn@users.noreply.github.com>
2021-01-14 18:20:25 +0900

    Update UpDownGame.java

--- JAVA Basic/src/UpDownGame.java
+++ JAVA Basic/src/UpDownGame.java
@@ -13,22 +13,21 @@ public class UpDownGame {
 		int answer=rand.nextInt(100);
 		System.out.println("A random number has been choiced. Guess this.(0-99)");
 		int i=1;
-	do {
-		
-		System.out.print(i+">> ");
-		guess= scanner.nextInt();
-		
-		if(guess>answer)
-			System.out.println("More lower.");
-		else if(guess<answer)
-			System.out.println("More higher.");
-		i+=1;
-	}
-	while(guess!=answer);
+		do {
+
+			System.out.print(i+">> ");
+			guess= scanner.nextInt();
+
+			if(guess>answer)
+				System.out.println("More lower.");
+			else if(guess<answer)
+				System.out.println("More higher.");
+			i+=1;
+		}
+		while(guess!=answer);
 	
-	System.out.print("Collect! The answer is "+answer+". Wanna again?(y/n)>> ");
-	again= scanner.next();
-	
+		System.out.print("Collect! The answer is "+answer+". Wanna again?(y/n)>> ");
+		again= scanner.next();
 	}
 	while(again.equals("y"));
 	}
