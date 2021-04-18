c22f5d955972067100742aaa45c15efc5550c15f
HongBeen <redbean-2@naver.com>
2021-04-18 18:18:25 +0900

    SuperEX.java 로 수정 완료
    기존 ColorPointEX.java를 super()를 활용한 상속방식 코드로 수정함

--- JAVA Basic/src/SuperEX.java
+++ JAVA Basic/src/SuperEX.java
@@ -1,7 +1,11 @@
 class Point{// why not 'public'? "should be declared in a file named 'Point.java'"
     //At outside look, this class is ColorPoint, so we have only one public class. I guess.?
     private int x,y;
-    void set(int x,int y){
+    Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+    Point(int x,int y){
         this.x=x;
         this.y=y;
     }
@@ -12,23 +16,18 @@ class Point{// why not 'public'? "should be declared in a file named 'Point.java
 
 class ColorPoint extends Point{
     private String color;
-    void setColor(String color){
+    ColorPoint (int x, int y,String color) {
+        super(x,y);
         this.color=color;
     }
     void showColor(){
-        System.out.println(color);
+        System.out.print(color);
         showPoint();
     }
 }
-public class ColorPointEx extends ColorPoint{
+public class SuperEX {
     public static void main(String [] args){
-        Point P=new Point();
-        P.set(2,5);
-        P.showPoint();
-
-        ColorPoint cp = new ColorPoint();
-        cp.set(3,4);
-        cp.setColor("orange");
+        ColorPoint cp=new ColorPoint(2,5,"blue");
         cp.showColor();
     }
 }
