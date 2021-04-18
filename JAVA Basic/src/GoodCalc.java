1078fc68e5490ee1ea01b72abc2439f892977f93
HongBeen <redbean-2@naver.com>
2021-04-18 23:23:35 +0900

    GoodCalc.java 생성
    추상클래스를 활용한 간단계산기

---
+++ JAVA Basic/src/GoodCalc.java
@@ -0,0 +1,29 @@
+abstract class Calculator{
+    public abstract int add(int a,int b);
+    public abstract int subtract(int a,int b);
+    public abstract double average(int[] a);
+}
+
+public class GoodCalc extends Calculator{
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int subtract(int a,int b){
+        return a-b;
+    }
+    public double average(int[] a){
+        double sum=0;
+        for (int i=0;i<a.length;i++){
+            sum+=a[i];
+        }
+        return sum/a.length;
+    }
+    public static void main(String[] args){
+        GoodCalc gc=new GoodCalc();
+        int a[]=new int[] {3,44,32,90,47};
+
+        System.out.println(gc.add(1,3));
+        System.out.println(gc.subtract(1,3));
+        System.out.println(gc.average(a));
+    }
+}
