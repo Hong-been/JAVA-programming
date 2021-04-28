30044fb76b2f4a97b2feb35e0a8227b6067a7e4a
HongBeen <redbean-2@naver.com>
2021-04-28 23:14:49 +0900

    .class 파일 업로드

--- JAVA Basic/src/InstanceOfEx.java
+++ JAVA Basic/src/InstanceOfEx.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class InstanceOfEx {
+class Person{}
+class Student extends Person{}
+class Researcher extends Person{}
+class Professor extends Researcher{}
+
+public class InstanceOfEx {
+    static void print(Person p) {
+        if (p instanceof Person) {
+            System.out.print("Person ");
+        }
+        if (p instanceof Student) {
+            System.out.print("Student ");
+        }
+        if (p instanceof Researcher) {
+            System.out.print("Researcher ");
+        }
+        if (p instanceof Professor) {
+            System.out.print("Professor ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        System.out.print("new Student() -> ");
+        print(new Student()); //레퍼런스를 지정하지않고(저장안할때) 임시로 객체만 생성함
+        System.out.print("new Researcher() -> ");
+        print(new Researcher());
+        System.out.print("new Professor() -> ");
+        print(new Professor());
+    }
 }
