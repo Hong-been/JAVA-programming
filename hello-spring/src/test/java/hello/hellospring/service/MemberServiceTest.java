366e1935d5bc8f9976245fd8d6e0c1cc0440da7c
HongBeen <redbean-2@naver.com>
2021-05-21 00:48:24 +0900

    MemberServiceTest.java, MemberService.java 회원저장소 DI형식으로 수정
    회원저장소 memberRepository를 MemberService클래스에서 new로 할당하지 않고,
    외부에서 넣어주는 Dependency Injection방식으로 수정

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,14 +1,32 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository=new MemoryMemberRepository();
+        memberService=new MemberService(memberRepository);
+    }
+
+
+    @AfterEach
+    void atferEach(){
+        memberRepository.clearStore();
+    }
+
     @Test
     void join() {
         //given
@@ -34,13 +52,17 @@ class MemberServiceTest {
 
         //when
         memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("This name has already existed here.");
+
+/*
         try{
             memberService.join(member2);
             fail();
         }catch(IllegalStateException e) {
             assertThat(e.getMessage()).isEqualTo("This name has already existed here.");
         }
-
+*/
         //then
     }
 
